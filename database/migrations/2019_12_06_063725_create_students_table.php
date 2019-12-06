00e2f3904562bfc76c9a0ad164d6cbc586dfe4da
eignerhandoko <eigner_handoko@yahoo.com>
2019-12-06 15:35:50 +0700

    Initial commit

---
+++ database/migrations/2019_12_06_063725_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('student_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('home address');
+            $table->string('phone_number');
+            $table->string('home_number');
+            $table->string('email');
+            $table->date('DOB');
+            $table->string('Place of Birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
